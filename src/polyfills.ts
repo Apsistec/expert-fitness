ed8faab011521e2a762836244b360ab1ce314a00
Doug White <doug@apsistec.app>
2020-11-20 19:44:20 -0600

    updates

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -17,7 +17,6 @@
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
-// import 'exceljs';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 import 'classlist.js';  // Run `npm install --save classlist.js`.
@@ -36,7 +35,7 @@ import 'web-animations-js';  // Run `npm install --save web-animations-js`.
  * will put import in the top of bundle, so user need to create a separate file
  * in this directory (for example: zone-flags.ts), and put the following flags
  * into that file, and then add the following code before importing zone.js.
- * import './zone-flags';
+ * import './zone-flags.ts';
  *
  * The flags allowed in zone-flags.ts are listed here.
  *
@@ -44,18 +43,21 @@ import 'web-animations-js';  // Run `npm install --save web-animations-js`.
  *
  * (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
  * (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
- * (window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
+ * (window as any).__zone_symbol__BLACK_LISTED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
  *
  *  in IE/Edge developer tools, the addEventListener will also be wrapped by zone.js
  *  with the following flag, it will bypass `zone.js` patch for IE/Edge
  *
+ *  (window as any).__Zone_enable_cross_context_check = true;
  *
  */
-(window as any).__Zone_enable_cross_context_check = true;
+
+import './zone-flags';
 
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
+
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 
